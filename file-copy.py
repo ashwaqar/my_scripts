378ebd78fb3e3ab3321756189df973e4163f3b42
Ashwaq <ashwaqar@users.noreply.github.com>
2019-08-22 16:55:56 +0530

    added new working script

--- file-copy.py
+++ file-copy.py
@@ -1,30 +1,48 @@
-"""We are going to save a txt file with all the files in your directory.
-We will use the function walk()
-
-"""
+#!/usr/bin/python
 
 import os
+import fnmatch
+import argparser
+import io
 
-# see all the methos of os
-# print(*dir(os), sep=", ")
-listafile = []
-percorso = []
-with open("lista_file.txt", "w") as testo:
-    for root, dirs, files in os.walk("."):
-        for file in files:
-            listafile.append(file)
-            percorso.append(root + "\\" + file)
-            testo.write(file + "\n")
-listafile.sort()
-print("N. of files", len(listafile))
-with open("lista_file_ordinata.txt", "w") as testo_ordinato:
-    for file in listafile:
-        testo_ordinato.write(file + "\n")
+# Create arguments to pass over to the command.
 
-with open("percorso.txt", "w") as file_percorso:
-    for file in percorso:
-        file_percorso.write(file + "\n")
+parser = argparse.ArgumentParser(
+    description="Script to Find, Filter & copy files.",
+    epilog="Please refer the above syntax for running the script correctly")
 
-os.system("lista_file.txt")
-os.system("lista_file_ordinata.txt")
-os.system("percorso.txt")
+parser.add_argument("-n", "--file_name", nargs='?', help="Pass the file name to search.")
+parser.add_argument("-s", "--string", nargs='?', help="Pass the string text to search in a file.")
+parser.add_argument("-i", "--input_path", nargs='?', help="Path where to execute the script or where files are present")
+parser.add_argument("-o", "--out_dir", nargs='?', help="Path where files need to be copied.")
+
+args = parser.parse_args()
+
+# Main script
+
+# Append a directory separator if not already present
+if not (args.input_path.endswith("/") or args.input_path.endswith("\\")):
+    args.input_path = args.input_path + "/"
+
+# If path does not exist, set search path to current directory
+if not os.path.exists(args.input_path):
+    args.input_path = "."
+
+# Search based on string text in a file
+if args.string:
+    for file in os.listdir(args.input_path):
+        # Read only files and ignore direcories
+        if os.path.isfile(args.input_path+file):
+            # Open file for reading
+            # Use either encoding='latin-1' or errors='ignore'
+            # Using io to avoid encoding invalid errors
+            with io.open(args.input_path + file, encoding='latin-1') as fo:
+                if args.string in fo.read():
+                    print(file)
+# Search based on file name
+elif args.file_name:
+    for file in os.listdir(args.input_path):
+        if os.path.isfile(file):
+            # match the file name based on the string text given
+            if fnmatch.fnmatch(file, '*' + args.file_name + '*'):
+                print(file)
