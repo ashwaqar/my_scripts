e03685b9fc78ce9c7e24c5d2238acf209c64607c
Ashwaq <ashwaqar@users.noreply.github.com>
2019-08-26 14:41:06 +0530

    shell list & copy script

---
+++ list.sh
@@ -0,0 +1,11 @@
+ls -lrth
+ls -lrth | wc -l
+ls -lrth > shell_results.txt
+ls -lrth | wc -l >> shell_results.txt
+echo "Results saved to shell_results.txt"
+cat shell_results.txt
+
+for f in *.txt
+do
+   cp -v "$f" testing_copy/"${f%.txt}"$(date +%m%d%y).txt
+done
